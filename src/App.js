c9f67dbc9922d8be8e7a3a2cd49db5affcef858d
sahil <st83210@gmail.com>
2023-04-08 15:49:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import { Card, Contactus, Header, Sectionfive, Sectionfour, Sectionone, Sectionsix, Sectionthree, Sectiontwo } from "./components";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Header/>
+<Sectionone/>
+<br/>
+<Contactus/>
+<br/>
+<Sectiontwo/>
+<br/>
+<Card/>
+<br/>
+<Sectionthree/>
+<br/>
+<Sectionfour/>
+<br/>
+<Sectionfive/>
+<br/>
+<Sectionsix/>
+    </>
   );
 }
 
